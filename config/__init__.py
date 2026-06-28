c5067a049dd0717ba7a301252f4a0796836b4b8c
g-ishika <2024ugme051@nitjsr.ac.in>
2026-06-29 01:27:07 +0530

    feat: add yaml config files for project settings

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,5 @@
 """Configuration management module for environmental monitoring system"""
 
-from .config import load_config, Config
+from config import load_config, Config
 
 __all__ = ['load_config', 'Config']
